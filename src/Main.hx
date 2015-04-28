dd3b57afe81df8dd8ce479e56b9188be439dd609
HawkHunt <cobraluuk@hotmail.com>
2015-04-28 14:07:07 +0200

    Tilesheet integration

--- src/Main.hx
+++ src/Main.hx
@@ -3,6 +3,11 @@ package ;
 import flash.display.Sprite;
 import flash.events.Event;
 import flash.Lib;
+import openfl.display.BitmapData;
+import openfl.Assets;
+import openfl.display.Bitmap;
+import openfl.display.Tilesheet;
+import openfl.geom.Rectangle;
 
 /**
  * ...
@@ -12,6 +17,7 @@ import flash.Lib;
 class Main extends Sprite 
 {
 	var inited:Bool;
+	var tilesheet:Tilesheet;
 
 	/* ENTRY POINT */
 	
@@ -25,15 +31,41 @@ class Main extends Sprite
 	{
 		if (inited) return;
 		inited = true;
-
+	}
 		// (your code here)
+		
+	private function allTiles()
+	{
+		var tilesBitmapData:BitmapData = Assets.getBitmapData("img/tileMap.png");
+		tilesheet = new Tilesheet(tilesBitmapData);/*create var*/
+		
+		var x = 0;
+		var y = 0;
+		var i = 0;
+		var t = 0;
+		while (i < 257)
+		{
+			while (t < 16)
+			{
+				tilesheet.addTileRect( new Rectangle (x, y, 32, 32));
+				x += 32;
+				t ++;
+			}
+			y += 32;
+			x = 0;
+			t = 0;
+			i++;
+		}
+		
+	
+	}
 		
 		// Stage:
 		// stage.stageWidth x stage.stageHeight @ stage.dpiScale
 		
 		// Assets:
 		// nme.Assets.getBitmapData("img/assetname.jpg");
-	}
+	
 
 	/* SETUP */
 
