2dadb981851b75af952c18f09da82a83422ed0ff
7052roy <leinenga.r@hotmail.nl>
2015-05-26 14:47:05 +0200

    Flixel testing
    added a new project with some flixel tests

---
+++ Flixel testing/FlixelTut/source/PlayState.hx
@@ -0,0 +1,76 @@
+package;
+
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.tile.FlxTilemap;
+import flixel.ui.FlxButton;
+import flixel.util.FlxAngle;
+import flixel.util.FlxDestroyUtil;
+import flixel.util.FlxMath;
+import flixel.util.FlxPoint;
+
+/**
+ * A FlxState which can be used for the actual gameplay.
+ */
+class PlayState extends FlxState
+{
+	
+	private var _player:Player;
+	private var _map:FlxOgmoLoader;
+	private var _mWalls:FlxTilemap;
+	
+	/**
+	 * Function that is called up when to state is created to set it up. 
+	 */
+	override public function create():Void
+	{
+		_map = new FlxOgmoLoader("assets/data/room-001.oel");
+		_mWalls = _map.loadTilemap("assets/images/Tilesheet_Complete.png", 64, 64, "tree");
+		_mWalls.setTileProperties(0, FlxObject.NONE);
+		_mWalls.setTileProperties(1, FlxObject.ANY);
+		_mWalls.setTileProperties(3, FlxObject.NONE);
+		add(_mWalls);
+		
+		_player = new Player();
+		_map.loadEntities(placeEntities, "entities");
+		add(_player);
+		FlxG.camera.follow(_player, FlxCamera.STYLE_TOPDOWN, 1);
+		
+		super.create();	
+		
+	}
+	
+	private function placeEntities(entityName:String, entityData:Xml):Void
+	{
+		if (entityName == "player")
+		{
+			_player.x = Std.parseInt(entityData.get("x"));
+			_player.y = Std.parseInt(entityData.get("y"));
+		}
+	}
+	
+	
+	/**
+	 * Function that is called when this state is destroyed - you might want to 
+	 * consider setting all objects this state uses to null to help garbage collection.
+	 */
+	override public function destroy():Void
+	{
+		super.destroy();
+	}
+	
+	/**
+	 * Function that is called once every frame.
+	 */
+	override public function update():Void
+	{
+		super.update();
+		
+		FlxG.collide(_player, _mWalls);
+	}	
+}
---
+++ Flixel testing/FlixelTut/source/Player.hx
@@ -0,0 +1,115 @@
+package ;
+
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.util.FlxAngle;
+import flixel.util.FlxColor;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.util.FlxAngle;
+import flixel.FlxG;
+
+class Player extends FlxSprite
+{
+
+	public var speed:Float = 200;
+	
+	public function new(X:Float=0, Y:Float=0) 
+	{
+		super(X, Y);
+		loadGraphic("assets/images/tec.png", true, 64, 64);
+		setFacingFlip(FlxObject.LEFT, false, false);
+		setFacingFlip(FlxObject.RIGHT, true, false);
+		animation.add("d", [0, 1, 2, 3], 5, false);
+		animation.add("lr", [8, 9, 10, 11], 5, false);
+		animation.add("u", [4, 5, 7, 7], 5, false);
+		drag.x = drag.y = 1600;
+		width = 8;
+		height = 14;
+		offset.x = 4;
+		offset.y = 2;
+		
+	}
+	
+	override public function draw():Void 
+	{
+		if (velocity.x != 0 || velocity.y != 0)
+		{
+			switch(facing)
+			{
+				case FlxObject.LEFT, FlxObject.RIGHT:
+					animation.play("lr");
+					
+				case FlxObject.UP:
+					animation.play("u");
+					
+				case FlxObject.DOWN:
+					animation.play("d");
+			}
+		}
+			
+		super.draw();
+	}
+	
+	private function movement():Void
+	{
+		var _up:Bool = false;
+		var _down:Bool = false;
+		var _left:Bool = false;
+		var _right:Bool = false;
+		
+		_up = FlxG.keys.anyPressed(["UP", "W"]);
+		_down = FlxG.keys.anyPressed(["DOWN", "S"]);
+		_left = FlxG.keys.anyPressed(["LEFT", "A"]);
+		_right = FlxG.keys.anyPressed(["RIGHT", "D"]);
+		
+		if (_up && _down)
+			_up = _down = false;
+		if (_left && _right)
+			_left = _right = false;
+		
+		if ( _up || _down || _left || _right)
+		{
+			var mA:Float = 0;
+			if (_up)
+			{
+				mA = -90;
+				if (_left)
+					mA -= 45;
+				else if (_right)
+					mA += 45;
+					
+				facing = FlxObject.UP;
+			}
+			else if (_down)
+			{
+				mA = 90;
+				if (_left)
+					mA += 45;
+				else if (_right)
+					mA -= 45;
+				
+				facing = FlxObject.DOWN;
+			}
+			else if (_left)
+			{
+				mA = 180;
+				facing = FlxObject.LEFT;
+			}
+			else if (_right)
+			{
+				mA = 0;
+				facing = FlxObject.RIGHT;
+			}
+			FlxAngle.rotatePoint(speed, 0, 0, 0, mA, velocity);
+		}
+	}
+	
+	override public function update():Void 
+	{
+		movement();
+		super.update();
+	}
+	
+}
