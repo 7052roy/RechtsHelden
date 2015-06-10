269a3ee06f75db7328962d456647528012aabd92
7052roy <leinenga.r@hotmail.nl>
2015-06-10 14:39:56 +0200

    project change

---
+++ source/PlayState.hx
@@ -0,0 +1,172 @@
+package;
+
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.group.FlxGroup;
+import flixel.group.FlxTypedGroup;
+import flixel.system.FlxSound;
+import flixel.text.FlxText;
+import flixel.tile.FlxTilemap;
+import flixel.ui.FlxButton;
+import flixel.ui.FlxVirtualPad;
+import flixel.util.FlxAngle;
+import flixel.util.FlxColor;
+import flixel.util.FlxDestroyUtil;
+import flixel.util.FlxMath;
+import flixel.util.FlxPoint;
+import flixel.util.FlxTimer;
+
+/**
+ * A FlxState which can be used for the actual gameplay.
+ */
+class PlayState extends FlxState
+{
+	
+	private var _player:Player;
+	private var _map:FlxOgmoLoader;
+	private var _mWalls:FlxTilemap;
+	private var _grpCoins:FlxTypedGroup<Coin>;
+	private var _grpEnemies:FlxTypedGroup<Enemy>;
+	var townMusic:FlxSound;
+	private var _hud:HUD;
+	private var _money:Int = 0;
+	private var _health:Int = 3;
+	private var _inCombat:Bool = false;
+	private var _teacher:Teacher;
+	var _interaction:Bool = false;
+	
+	#if mobile
+	public static var virtualPad:FlxVirtualPad;
+	#end
+	
+	/**
+	 * Function that is called up when to state is created to set it up. 
+	 */
+	override public function create():Void
+	{
+		_map = new FlxOgmoLoader("assets/data/basemap1.oel");
+		_mWalls = _map.loadTilemap("assets/images/Tilesheet_Complete.png", 64, 64, "tree");
+		_mWalls.setTileProperties(1, FlxObject.ANY);
+		_mWalls.setTileProperties(3, FlxObject.NONE);
+		_mWalls.setTileProperties(2, FlxObject.NONE);
+		add(_mWalls);
+		
+		_grpCoins = new FlxTypedGroup<Coin>();
+		add(_grpCoins);
+		
+		_grpEnemies = new FlxTypedGroup<Enemy>();
+		add(_grpEnemies);
+		
+		_player = new Player();
+		add(_player);
+		
+		_teacher = new Teacher();
+		add(_teacher);
+		
+		_map.loadEntities(placeEntities, "entities");
+		
+		
+		
+		
+		FlxG.camera.follow(_player, FlxCamera.STYLE_TOPDOWN, null, 1);
+		
+		//_hud = new HUD();
+		//add(_hud);
+		townMusic = FlxG.sound.load("assets/music/townMusic.wav");
+		townMusic.play(true);
+		
+		FlxG.camera.fade(FlxColor.BLACK, 2, true);
+		
+		#if mobile
+		virtualPad = new FlxVirtualPad(FULL, NONE);		
+		add(virtualPad);
+		#end
+		
+		super.create();	
+		
+	}
+	
+	private function placeEntities(entityName:String, entityData:Xml):Void
+	{
+		if (entityName == "player")
+		{
+			_player.x = Std.parseInt(entityData.get("x"));
+			_player.y = Std.parseInt(entityData.get("y"));
+		}
+		else if (entityName == "coin") 
+		{
+			_grpCoins.add(new Coin(Std.parseInt(entityData.get("x")) + 4, Std.parseInt(entityData.get("y")) + 4));
+			
+		}
+		else if (entityName == "enemy")
+		{
+			_grpEnemies.add(new Enemy(Std.parseInt(entityData.get("x"))+4, Std.parseInt(entityData.get("y")), Std.parseInt(entityData.get("etype"))));
+		}
+		else if (entityName == "mission1_Teacher")
+		{
+			_teacher.x = Std.parseInt(entityData.get("x"));
+			_teacher.y = Std.parseInt(entityData.get("y"));
+		}
+	}
+	
+	
+	/**
+	 * Function that is called when this state is destroyed - you might want to 
+	 * consider setting all objects this state uses to null to help garbage collection.
+	 */
+	override public function destroy():Void
+	{
+		super.destroy();
+	}
+
+	/**
+	 * Function that is called once every frame.
+	 */
+	override public function update():Void
+	{
+		super.update();
+		_player.speed = 300;
+		FlxG.collide(_player, _mWalls);
+		FlxG.overlap(_player, _grpCoins, playerTouchCoin);
+		FlxG.collide(_grpEnemies, _mWalls);
+		FlxG.overlap(_player, _teacher, loadMission1);
+		FlxG.collide(_teacher, _mWalls);
+		FlxG.collide(_player, _grpEnemies, playerEnemy);
+	}	
+	
+	
+	function loadMission1(p:Player, t:Teacher)
+	{
+		
+		_interaction = FlxG.keys.anyPressed(["q", "Q"]);
+		if (_interaction)
+		{
+			townMusic.stop();
+			FlxG.switchState(new Mission1());
+		}
+		
+	}
+	
+	function playerEnemy(p:Player, e:Enemy)
+	{
+		p.speed = 0;
+		e.speed = 0;
+		e.velocity.x = 0;
+		e.velocity.y = 0;
+	}
+	
+	
+	private function playerTouchCoin(P:Player, C:Coin):Void
+	{
+		if (P.alive && P.exists && C.alive && C.exists)
+		{
+			C.kill();
+			_money++;
+			//_hud.updateHUD(_health, _money);
+		}
+	}
+}
